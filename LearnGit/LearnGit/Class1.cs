4510307549b4f1f44f5f9676f4bba03ee70755f3
MohammadAminAlinasab <lynsbmhmd8@gmail.com>
2025-06-06 13:40:31 -0700

    does adding a single class affects?

---
+++ LearnGit/LearnGit/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Block;
+
+namespace Dorm
+{
+    internal class Dorm
+    {
+        public string Name { get; set; }
+        List<Block.Block> blocks = new List<Block.Block>();
+        public List<Block.Block> Blocks { get { return blocks; } set { blocks = value; } }
+    }
+}
